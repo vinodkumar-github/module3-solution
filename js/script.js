cc82dbd5026152335171cd68abfac2d8e1184cad
vinodkumar-github <140242380+vinodkumar-github@users.noreply.github.com>
2023-08-28 00:39:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-$(function () { // Same as document. addEventListener ("DOMContentLoaded"
-    // Same as document. querySelector ("#navbarToggle") .addEventListener ("bI
-    $(".navbar-toggle").blur(function(event) {
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 768) {
-    $("#collapse-1").collapse('hide');
-    }
-});
+$(function() {
+    // Same as document.addEventListener("DOMContentLoaded"
+    // Same as document.querySelector("#navbarToggle").addEventListener("click"
+    $("#button1").blur(function(event) {
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#collapse-1").collapse('hide');
+        }
+    });
 });
